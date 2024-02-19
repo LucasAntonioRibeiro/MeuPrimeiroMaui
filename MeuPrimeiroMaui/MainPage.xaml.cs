2b6893697fcb41b1bfe661ea399014c41bdc4e58
Usuario <Usuario@CPD03-M12>
2024-02-19 12:21:46 -0300

    começar a fazer a equação

--- MeuPrimeiroMaui/MainPage.xaml.cs
+++ MeuPrimeiroMaui/MainPage.xaml.cs
@@ -1,24 +1,38 @@
-﻿namespace meuPrimeiroMaui
+﻿using Microsoft.UI.Xaml.Controls.Primitives;
+
+namespace meuPrimeiroMaui
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
 
         public MainPage()
         {
             InitializeComponent();
         }
 
-        private void OnCounterClicked(object sender, EventArgs e)
+        private void Button_Clicked(object sender, EventArgs e)
         {
-            count++;
+            double a = Convert.ToDouble(txt_a.Text); 
+            double b = Convert.ToDouble(txt_b.Text);
+            double c = Convert.ToDouble(txt_c.Text);
 
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
+            double x1 = 0, x2 = 0;
 
-            SemanticScreenReader.Announce(CounterBtn.Text);
+            string msg = $"O resultado é {x1} e {x2}";
+
+            double delta = (b * b) - (4 * a * c);
+
+            if (delta = 0)
+            {
+                 x1 = (-b + Math.Sqrt(delta)) /2*a;
+
+
+                DisplayAlert("Resultado", msg, "Fechar");
+            }
+
+            
+             x2 = -b - Math.Sqrt(delta) / 2 * a;
+
         }
     }
 
