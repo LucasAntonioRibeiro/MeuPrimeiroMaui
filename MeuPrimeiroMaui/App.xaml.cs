30c4ed8f03bb975bd03c23ce17174d2339732dab
Usuario <Usuario@CPD03-M12>
2024-02-19 11:32:29 -0300

    Adicionar arquivos de projeto.

---
+++ MeuPrimeiroMaui/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace meuPrimeiroMaui
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
